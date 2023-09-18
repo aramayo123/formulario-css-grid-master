a19187d21761b90be696341ca666dd4b0a4a0b08
aramayo123 <aramayo420@gmail.com>
2023-09-18 17:57:04 -0300

    a

---
+++ php/conexion.php
@@ -0,0 +1,20 @@
+<?php
+    
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "pruebajs";
+    /*
+    $server = "sql300.epizy.com";
+    $user = "epiz_32074090";
+    $password = "QABHKcesgw";
+    $database = "epiz_32074090_gym";
+    */
+    $conn = mysqli_connect($server,$user,$password,$database);
+    /*
+    if($conn){
+        echo "conexion exitosa";
+    }else{
+        echo "conexion fallida";
+    }*/
+?>
